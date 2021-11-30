c5e082e3099ddb63544ee7571684a02a44dc1ad8
jujin1324 <jujin1324@daum.net>
2021-11-30 21:42:48 +0900

    Test 리팩토링

--- src/test/java/practice/exceptionstarter/controllers/api/ApiExceptionControllerTest.java
+++ src/test/java/practice/exceptionstarter/controllers/api/ApiExceptionControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import practice.exceptionstarter.controllers.advice.ExceptionControllerAdvice;
@@ -55,51 +57,43 @@ class ApiExceptionControllerTest {
     @Test
     @DisplayName("[비지니스 로직 에러] 존재하지 않는 사용자")
     void getMember_whenNotExistUser_thenNotFound() throws Exception {
-        mockMvc.perform(get("/api/members/{id}", MEMBER_ID_NOT_EXIST)
-                        .accept(MediaType.APPLICATION_JSON)
-                )
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.code").value("Not Found"))
-                .andExpect(jsonPath("$.message").value(ErrorMessageConst.RESOURCE_NOT_FOUND + " [member]"))
-                .andExpect(jsonPath("$.error").value(ErrorConst.RESOURCE_NOT_FOUND));
+        req_notFound(
+                get("/api/members/{id}", MEMBER_ID_NOT_EXIST),
+                ErrorConst.RESOURCE_NOT_FOUND,
+                ErrorMessageConst.RESOURCE_NOT_FOUND + " [member]"
+        );
     }
 
     @Test
     @DisplayName("[비지니스 로직 에러] 중복된 사용자 조회")
     void getMember_whenNotDuplicatedUser_thenBadRequest() throws Exception {
-        mockMvc.perform(get("/api/members/{id}", MEMBER_ID_DUPLICATED)
-                        .accept(MediaType.APPLICATION_JSON)
-                )
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.code").value("Bad Request"))
-                .andExpect(jsonPath("$.message").value(ErrorMessageConst.RESOURCE_DUPLICATED + " [member]"))
-                .andExpect(jsonPath("$.error").value(ErrorConst.RESOURCE_DUPLICATED));
+        req_badRequest(
+                get("/api/members/{id}", MEMBER_ID_DUPLICATED),
+                ErrorConst.RESOURCE_DUPLICATED,
+                ErrorMessageConst.RESOURCE_DUPLICATED + " [member]"
+        );
     }
 
     @Test
     @DisplayName("[타입 에러] PathVariable 타입 에러")
     void getMember_whenTypeMismatchPathVariable() throws Exception {
-        mockMvc.perform(get("/api/members/{memberId}", MEMBER_ID_TYPE_MISMATCH)
-                        .accept(MediaType.APPLICATION_JSON)
-                )
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.code").value("Bad Request"))
-                .andExpect(jsonPath("$.message")
-                        .value(String.format("memberId [%s] 의 타입이 올바르지 않습니다. 올바른 타입은 class java.lang.Long 입니다.", MEMBER_ID_TYPE_MISMATCH)))
-                .andExpect(jsonPath("$.error").value(ErrorConst.TYPE_MISMATCH));
+        req_badRequest(
+                get("/api/members/{memberId}", MEMBER_ID_TYPE_MISMATCH),
+                ErrorConst.TYPE_MISMATCH,
+                String.format("memberId [%s] 의 타입이 올바르지 않습니다. 올바른 타입은 class java.lang.Long 입니다.",
+                        MEMBER_ID_TYPE_MISMATCH)
+        );
     }
 
     @Test
     @DisplayName("[타입 에러] RequestParam 타입 에러")
     void getMember_whenTypeMismatchRequestParam() throws Exception {
-        mockMvc.perform(get("/api/members/requestParam?memberId={memberId}", MEMBER_ID_TYPE_MISMATCH)
-                        .accept(MediaType.APPLICATION_JSON)
-                )
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.code").value("Bad Request"))
-                .andExpect(jsonPath("$.message")
-                        .value(String.format("memberId [%s] 의 타입이 올바르지 않습니다. 올바른 타입은 class java.lang.Long 입니다.", MEMBER_ID_TYPE_MISMATCH)))
-                .andExpect(jsonPath("$.error").value(ErrorConst.TYPE_MISMATCH));
+        req_badRequest(
+                get("/api/members/requestParam?memberId={memberId}", MEMBER_ID_TYPE_MISMATCH),
+                ErrorConst.TYPE_MISMATCH,
+                String.format("memberId [%s] 의 타입이 올바르지 않습니다. 올바른 타입은 class java.lang.Long 입니다.",
+                        MEMBER_ID_TYPE_MISMATCH)
+        );
     }
 
     @Test
@@ -109,13 +103,32 @@ class ApiExceptionControllerTest {
         reqBody.put("name", "test");
         reqBody.put("age", "invalid");  /* age 는 Integer 인데 String 을 넣었다. */
 
-        mockMvc.perform(post("/api/members")
+        req_badRequest(
+                post("/api/members")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(reqBody))
-                )
+                        .content(objectMapper.writeValueAsString(reqBody)),
+                ErrorConst.INVALID_REQUEST_BODY,
+                null
+        );
+    }
+
+    private void req_badRequest(MockHttpServletRequestBuilder builder, String error, String message) throws Exception {
+        ResultActions resultActions = mockMvc.perform(builder)
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code").value("Bad Request"))
-                .andExpect(jsonPath("$.error").value(ErrorConst.INVALID_REQUEST_BODY));
+                .andExpect(jsonPath("$.error").value(error));
+        if (message != null) {
+            resultActions
+                    .andExpect(jsonPath("$.message").value(message));
+        }
+    }
+
+    private void req_notFound(MockHttpServletRequestBuilder builder, String error, String message) throws Exception {
+        mockMvc.perform(builder)
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code").value("Not Found"))
+                .andExpect(jsonPath("$.error").value(error))
+                .andExpect(jsonPath("$.message").value(message));
     }
 }
